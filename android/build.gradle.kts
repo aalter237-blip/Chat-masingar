837feca3c8587f8f19dab43f5f8d05371fd389dc
aalter237-blip <you@example.com>
2026-09-03 04:37:42 +0300

    استبدال كامل بمحتوى masingar-lite-bonto

--- android/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    // نفس الإصدارات المجرّبة سابقاً على CI هذا المستودع (Gradle 8.9 في الـ workflow)
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-}
